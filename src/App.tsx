57a163da516573ef5c4321f784097db017df03aa
fuxuewei <476541140@qq.com>
2019-08-07 15:39:05 +0800

    ts + antd + react-app-rewired + babel-plugin-import

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Button} from 'antd';
+import './App.css';
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <Button type="primary">button</Button>
+    </div>
+  );
+}
+
+export default App;
